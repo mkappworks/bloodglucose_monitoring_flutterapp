4970ac5d7a32b62c20990a34af0c5a6d901957b5
Malith Kuruppu <info.mkappworks@gmail.com>
2021-07-10 12:00:24 +0530

    Added GlucoseContainer widget

---
+++ lib/view/home/components/glucose_container.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:bloodglucose_monitoring_flutterapp/controller/glucose_controller.dart';
+
+import 'package:bloodglucose_monitoring_flutterapp/utils/sized_box_functions.dart';
+
+import 'package:bloodglucose_monitoring_flutterapp/view/home/components/glucose_line_chart.dart';
+import 'package:bloodglucose_monitoring_flutterapp/view/home/components/parameter_card.dart';
+import 'package:bloodglucose_monitoring_flutterapp/view/home/components/parameter_gesture_card.dart';
+
+class GlucoseContainer extends StatelessWidget {
+  final GlucoseController _glucoseController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        addVerticalSpace(25.0),
+        GlucoseLineChart(),
+        addVerticalSpace(25.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ParameterGestureCard(
+              onLongPressFn: () =>
+                  _glucoseController.setIsMaximumGlucoseValueHover(),
+              onLongPressEndFn: (details) =>
+                  _glucoseController.setIsMaximumGlucoseValueHover(),
+              label:
+                  'Maximum : ${_glucoseController.getMaximumGlucoseValue[0].value}',
+            ),
+            ParameterGestureCard(
+                onLongPressFn: () =>
+                    _glucoseController.setIsMinimumGlucoseValueHover(),
+                onLongPressEndFn: (details) =>
+                    _glucoseController.setIsMinimumGlucoseValueHover(),
+                label:
+                    'Minimum : ${_glucoseController.getMinimumGlucoseValue[0].value}'),
+          ],
+        ),
+        addVerticalSpace(25.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ParameterCard(
+              label:
+                  'Average : ${_glucoseController.getAverageGlucoseValue[0]}',
+            ),
+            ParameterCard(
+              label: 'Median : ${_glucoseController.getMedianGlucoseValue[0]}',
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
