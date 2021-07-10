8a115d016b7251995723c74468e681ef01a1c4fa
Malith Kuruppu <info.mkappworks@gmail.com>
2021-07-10 11:58:05 +0530

    Added GlucoseController GetX class

---
+++ lib/controller/glucose_controller.dart
@@ -0,0 +1,88 @@
+import 'package:bloodglucose_monitoring_flutterapp/controller/helper/glucose_utils_helper.dart';
+import 'package:get/get.dart';
+
+import 'package:bloodglucose_monitoring_flutterapp/controller/helper/glucose_service_helper.dart';
+
+import 'package:bloodglucose_monitoring_flutterapp/model/glucose.dart';
+
+enum GlucoseListStatus { loading, loaded, empty }
+
+class GlucoseController extends GetxController {
+  Rx<GlucoseListStatus> _status = GlucoseListStatus.loading.obs;
+  RxList<Glucose> _glucoseList = <Glucose>[].obs;
+  RxList<Glucose> _minimumGlucoseValue = <Glucose>[].obs;
+  RxList<Glucose> _maximumGlucoseValue = <Glucose>[].obs;
+  RxList<double> _averageGlucoseValue = <double>[].obs;
+  RxList<double> _medianGlucoseValue = <double>[].obs;
+  RxBool _isMaximumGlucoseValueHover = false.obs;
+  RxBool _isMinimumGlucoseValueHover = false.obs;
+
+  Future<void> onInit() async {
+    super.onInit();
+    await _setGlucoseList();
+  }
+
+  //Function to get and set all raw glucose data external api
+  Future<void> _setGlucoseList() async {
+    _status.value = GlucoseListStatus.loading;
+
+    _glucoseList
+        .assignAll(await GlucoseServiceHelper.shared.fetchGlucoseList());
+
+    if (_glucoseList.isEmpty) {
+      _status.value = GlucoseListStatus.empty;
+    } else {
+      setGlucosseParameters();
+      _status.value = GlucoseListStatus.loaded;
+    }
+
+    update();
+  }
+
+ //Function to calculate and set all max, min, average and median glucose value from _glucoseList
+  void setGlucosseParameters() {
+    //calculating the maximum glucose value
+    _maximumGlucoseValue.assign(
+        GlucoseUtilsHelper.shared.calculateMaximumGlucoseValue(_glucoseList));
+
+    //calculating the minimum glucose value
+    _minimumGlucoseValue.assign(
+        GlucoseUtilsHelper.shared.calculateMinimumGlucoseValue(_glucoseList));
+
+    // calculating the average glucose value
+    _averageGlucoseValue.assign(
+        GlucoseUtilsHelper.shared.calculateAverageGlucoseValue(_glucoseList));
+
+    // calculating the median glucose value
+    _medianGlucoseValue.assign(
+        GlucoseUtilsHelper.shared.calculateMedianGlucoseValue(_glucoseList));
+
+    update();
+  }
+
+  //Function to set whether the Maximum Glucose Value is being long pressed
+  void setIsMaximumGlucoseValueHover() =>
+      _isMaximumGlucoseValueHover.value = !_isMaximumGlucoseValueHover.value;
+
+  //Function to set whether the Minimum Glucose Value is being long pressed
+  void setIsMinimumGlucoseValueHover() {
+    _isMinimumGlucoseValueHover.value = !_isMinimumGlucoseValueHover.value;
+  }
+
+  //get the current status of the GlucoseController
+  Rx<GlucoseListStatus> get getStatus => _status;
+  //get the _glucoseList
+  RxList<Glucose> get getGlucoseList => _glucoseList;
+  //get the _minimumGlucoseValue
+  RxList<Glucose> get getMinimumGlucoseValue => _minimumGlucoseValue;
+  //get the _maximumGlucoseValue
+  RxList<Glucose> get getMaximumGlucoseValue => _maximumGlucoseValue;
+  //get the _averageGlucoseValue
+  RxList<double> get getAverageGlucoseValue => _averageGlucoseValue;
+  //get the _medianGlucoseValue
+  RxList<double> get getMedianGlucoseValue => _medianGlucoseValue;
+  //get the _isMaximumGlucoseValueHover
+  RxBool get getIsMaximumGlucoseValueHover => _isMaximumGlucoseValueHover;
+  //get the _isMinimumGlucoseValueHover
+  RxBool get getIsMinimumGlucoseValueHover => _isMinimumGlucoseValueHover;
+}
