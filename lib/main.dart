a9e43048983ff6bd7b62e6b043c33b24d3c86467
Malith Kuruppu <info.mkappworks@gmail.com>
2021-07-11 04:22:45 +0530

    Updated with deeplink

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:app_links/app_links.dart';
 
 import 'package:bloodglucose_monitoring_flutterapp/controller/glucose_controller.dart';
 
@@ -19,7 +20,48 @@ void main() {
   });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlucoseController _glucoseController = Get.find();
+  // late Uri? appLink;
+
+  @override
+  void initState() {
+    super.initState();
+    initDeepLinks();
+  }
+
+  void initDeepLinks() async {
+    final _appLinks = AppLinks(onAppLink: (Uri uri, String stringUri) {
+      setAppStartEndDate(stringUri);
+    });
+
+    final appLink = await _appLinks.getInitialAppLink();
+
+    if (appLink != null && appLink.hasFragment && appLink.fragment != '/') {
+      setAppStartEndDate(appLink.toString());
+    }
+  }
+
+  void setAppStartEndDate(String stringUri) {
+    if (stringUri.contains("glevels?start=") && stringUri.contains("&end=")) {
+      List<String> uriList =
+          stringUri.split("customscheme://mkappworks.com/bgm/glevels?start=");
+      List<String> _startEndDateTime = uriList[1].split("&end=");
+      if (_glucoseController.getStatus.value == GlucoseListStatus.loaded) {
+        _glucoseController
+            .setFilteredStartDate(DateTime.parse(_startEndDateTime[0]));
+        _glucoseController
+            .setFilteredEndDate(DateTime.parse(_startEndDateTime[1]));
+        print(_startEndDateTime);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
