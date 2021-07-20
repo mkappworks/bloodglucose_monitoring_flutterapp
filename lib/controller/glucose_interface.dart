8f5f789978c9330241ea74a28a0ae2ada94a7112
Malith Kuruppu <info.mkappworks@gmail.com>
2021-07-20 12:21:34 +0530

    Added GlucoseInterface

---
+++ lib/controller/glucose_interface.dart
@@ -0,0 +1,46 @@
+import 'package:bloodglucose_monitoring_flutterapp/model/glucose.dart';
+import 'package:get/get.dart';
+
+enum GlucoseListStatus { loading, loaded, empty }
+
+abstract class GlucoseInterface {
+  Future<void> setGlucoseList();
+
+  void setApiStartEndDate(List<Glucose> currentList);
+
+  void setFilteredStartDate(DateTime startDate);
+
+  void setFilteredEndDate(DateTime endDate);
+
+  void setDateFilteredGlucoseList();
+
+  void setGlucoseParameters(List<Glucose> currentList);
+
+  void setIsMaximumGlucoseValueHover();
+
+  void setIsMinimumGlucoseValueHover();
+
+  Future<void> postGlucoseData();
+
+  Future<void> saveGlucoseData();
+
+  Rx<GlucoseListStatus> get getStatus;
+
+  RxList<DateTime> get getFilteredStartDate;
+
+  RxList<DateTime> get getFilteredEndDate;
+
+  RxList<Glucose> get getDateFilteredGlucoseList;
+
+  RxList<Glucose> get getMinimumGlucoseValue;
+
+  RxList<Glucose> get getMaximumGlucoseValue;
+
+  RxList<double> get getAverageGlucoseValue;
+
+  RxList<double> get getMedianGlucoseValue;
+
+  RxBool get getIsMaximumGlucoseValueHover;
+
+  RxBool get getIsMinimumGlucoseValueHover;
+}
